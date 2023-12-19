14664e3a20936f0b99de0ed88ef078b1cb6d41d2
shubhambajare <shubhambajare8@gmail.com>
2023-12-19 20:56:00 +0530

    css fix and no data found message added

--- src/components/QuoteList.jsx
+++ src/components/QuoteList.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { QuoteCard } from './QuoteCard';
+import { Typography } from '@mui/material';
 
 export const QuoteList = () => {
 
@@ -16,6 +17,12 @@ export const QuoteList = () => {
                     category={quote.category}
                 />
             ))}
+
+            {
+                !quotes.length && <Typography sx={{ mb: 1.5, mt: 2, fontSize: 15, textAlign: "center" }} color="text.secondary">
+                    No result found
+                </Typography>
+            }
         </div>
     )
 }
